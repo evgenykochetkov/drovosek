8b812e229ffe8a503c42b1b37e09b035778c34d0
Николай Писарев <n.pisarev@hostcomm.ru>
2016-10-10 19:59:48 +0300

    title screen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,18 +2,33 @@ import * as React from "react";
 import {connect} from "react-redux";
 
 import { GameState } from '../types/model'
+import { UserInputAction } from "../types/index";
 import renderStateToString from '../renderStateToString'
 import {Button, ButtonType} from './Button'
 
-class App extends React.Component<{gameState: GameState}, {}> {
+interface AppProps {
+    gameState: GameState
+    startGame: () => void
+}
+
+class App extends React.Component<AppProps, {}> {
   render () {
-    const { gameState } = this.props
+    const { gameState, startGame } = this.props
+
+    if (gameState.gameStatus === 'TITLE_SCREEN') {
+        return (
+            <div>
+                press space to start
+                <Button buttonType={ButtonType.primary} onClick={startGame}>Start</Button>
+            </div>
+        )
+    }
 
     return (
       <div>
         <pre>{JSON.stringify(gameState, null, 4)}</pre>
         <pre>{renderStateToString(gameState)}</pre>
-        <Button buttonType={ButtonType.primary} onClick={ () => alert('hi!') }>Push me</Button>
+
       </div>
     )
   }
@@ -21,6 +36,11 @@ class App extends React.Component<{gameState: GameState}, {}> {
 
 const mapStateToProps = (state: GameState) => ({gameState: state})
 
+const mapDispathToProps = (dispatch) => ({
+    startGame: () => dispatch({type: 'USER_INPUT_START', payload: 'SELECT'} as UserInputAction)
+})
+
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    mapDispathToProps
 )(App)
--- src/configureStore.ts
+++ src/configureStore.ts
@@ -8,7 +8,7 @@ export default function configureStore (initialState?) {
   const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(createSagaMiddleware(rootSaga)),
     (<any>window).devToolsExtension
-      ? (<any>window).devToolsExtension({actionsBlacklist: 'GAME_LOOP_TICK'}) // TODO: typecheck this
+      ? (<any>window).devToolsExtension({actionsBlacklist: ['GAME_LOOP_TICK']}) // TODO: typecheck this
       : (f) => f
   ))
 
